ba602957893ab116a12c02cd8deb800eb706fd64
Nick Vigilante <nickvigilante@users.noreply.github.com>
2019-05-24 22:13:21 -0400

    Add files via upload

--- passgen.py
+++ passgen.py
@@ -46,24 +46,28 @@ class Config:
         return config
 
 
-class PWApp:
+class PWApp(Frame):
 
     def __init__(self, master):
+        Frame.__init__(self, master)
 
         #define configuration
         self.cfg = Config().cfg
         master.protocol("WM_DELETE_WINDOW", self.save_and_quit)
         master.title("PassGen")
+        self.vcscmd = (self.register(self.validate_symbols), '%S', '%s')
 
         # define frame containers
+        self.bg = "gray25"
+        self.fg = "gray90"
 
-        self.settings = LabelFrame(master, text="Settings", padx=5, pady=5)
+        self.settings = LabelFrame(master, text="Settings", padx=5, pady=5, bg=self.bg, fg=self.fg)
         self.settings.grid(row=0, column=0)
 
-        self.buttons = LabelFrame(master, text="Buttons", padx=5, pady=5)
+        self.buttons = LabelFrame(master, padx=6, pady=41, bg=self.bg, fg=self.fg, highlightcolor=self.bg, highlightbackground=self.bg, highlightthickness=0)
         self.buttons.grid(row=0, column=1)
 
-        self.output = LabelFrame(master, text = "Output", padx=5, pady=5)
+        self.output = LabelFrame(master, text = "Output", padx=79, pady=5, bg=self.bg, fg=self.fg)
         self.output.grid(row=1, column=0, columnspan = 2)
 
         self.declare_vars()
@@ -81,54 +85,58 @@ class PWApp:
 
     def create_elements(self):
         # implement form elements
-        self.lc = Checkbutton(self.settings, text="a-z", variable=self.use_lc)
+        self.lc = Checkbutton(self.settings, text="a-z", variable=self.use_lc, bg=self.bg, fg=self.fg)
         self.lc.grid(row=0, column=0)
 
-        self.uc = Checkbutton(self.settings, text="A-Z", variable=self.use_uc)
+        self.uc = Checkbutton(self.settings, text="A-Z", variable=self.use_uc, bg=self.bg, fg=self.fg)
         self.uc.grid(row=0, column=1)
 
-        self.num = Checkbutton(self.settings, text="0-9", variable=self.use_num)
+        self.num = Checkbutton(self.settings, text="0-9", variable=self.use_num, bg=self.bg, fg=self.fg)
         self.num.grid(row=0, column=2)
 
-        self.sym = Checkbutton(self.settings, text="!@#$", variable=self.use_sym, command=self.create_cs_rb)
+        self.sym = Checkbutton(self.settings, text="!@#$", variable=self.use_sym, command=self.create_cs_rb, bg=self.bg, fg=self.fg)
         self.sym.grid(row=0, column=3)
 
-        self.cs_opt_1 = Radiobutton(self.settings, text="Use custom symbols:", variable=self.use_cs, value=1)
+        self.cs_opt_1 = Radiobutton(self.settings, text="Use custom symbols:", variable=self.use_cs, value=1, bg=self.bg, fg=self.fg)
         self.cs_opt_1.grid(row=2, column=0, columnspan=2)
-        self.cs_opt_2 = Radiobutton(self.settings, text="Use default symbols", variable=self.use_cs, value=0)
+        self.cs_opt_2 = Radiobutton(self.settings, text="Use default symbols", variable=self.use_cs, value=0, bg=self.bg, fg=self.fg)
         self.cs_opt_2.grid(row=3, column=0, columnspan=2)
 
-        self.len_label = Label(self.settings, text="Password Length:")
+        self.len_label = Label(self.settings, text="Password Length:", bg=self.bg, fg=self.fg)
         self.len_label.grid(row=1, column=0, columnspan=2)
 
-        self.len = Entry(self.settings, text="Password Length", textvariable=self.pass_len)
+        self.len = Entry(self.settings, text="Password Length", textvariable=self.pass_len, bg=self.bg, fg=self.fg)
         self.len.grid(row=1, column=2, columnspan=2)
 
-        self.pwd_label = Label(self.output, text = "Password:")
+        self.pwd_label = Label(self.output, text = "Password:", bg=self.bg, fg=self.fg)
         self.pwd_label.grid(row=0, column=0)
 
-        self.pwd_field = Entry(self.output, text="Password", textvariable=self.pwd, width=30)
+        self.pwd_field = Entry(self.output, text="Password", textvariable=self.pwd, width=30, bg=self.bg, fg=self.fg)
         self.pwd_field.grid(row=0, column=1)
 
-        self.generate_button = Button(self.buttons, text="Generate Password", command=self.gen_pwd)
+        self.generate_button = Button(self.buttons, text="Generate Password", command=self.gen_pwd, bg=self.bg, fg="black", highlightcolor="black", highlightbackground=self.bg)
         self.generate_button.grid(row=0, column=0)
 
-        self.quit_button = Button(self.buttons, text="Save and Quit", fg="red", command=self.save_and_quit)
+        self.quit_button = Button(self.buttons, text="Save and Quit", fg="red", command=self.save_and_quit, bg=self.bg, activebackground=self.bg, highlightcolor="black", highlightbackground="black")
         self.quit_button.grid(row=1, column=0)
 
     def create_cs_rb(self):
         disable_sym_rb = "DISABLED" if not getboolean(int(self.use_sym.get())) else "NORMAL"
-        exec("self.cs_opt_1 = Radiobutton(self.settings, text='Use custom symbols:', variable=self.use_cs, value=1, command=self.create_cs_field, state={0})".format(disable_sym_rb))
+        exec("self.cs_opt_1 = Radiobutton(self.settings, text='Use custom symbols:', variable=self.use_cs, value=1, command=self.create_cs_field, state={0}, bg=self.bg, fg=self.fg)".format(disable_sym_rb))
         self.cs_opt_1.grid(row=2, column=0, columnspan=2)
-        exec("self.cs_opt_2 = Radiobutton(self.settings, text='Use default symbols', variable=self.use_cs, value=0, command=self.create_cs_field, state={0})".format(disable_sym_rb))
+        exec("self.cs_opt_2 = Radiobutton(self.settings, text='Use default symbols', variable=self.use_cs, value=0, command=self.create_cs_field, state={0}, bg=self.bg, fg=self.fg)".format(disable_sym_rb))
         self.cs_opt_2.grid(row=3, column=0, columnspan=2)
         self.create_cs_field()
 
     def create_cs_field(self):
+
         disable_cs_field = "NORMAL" if getboolean(int(self.use_sym.get())) and getboolean(int(self.use_cs.get())) else "DISABLED"
-        exec('self.cs = Entry(self.settings, text="Custom Symbols:", textvariable=self.cust_sym_dec, state={0})'.format(disable_cs_field))
+        exec('self.cs = Entry(self.settings, text="Custom Symbols:", textvariable=self.cust_sym_dec, state={0}, validate="all", validatecommand=self.vcscmd, bg=self.bg, fg=self.fg, disabledforeground="gray30", disabledbackground="gray20")'.format(disable_cs_field))
         self.cs.grid(row=2, column=2, columnspan=2)
 
+    def validate_symbols(self, S, s):
+        result = (S not in self.lc_set.get()[:] + self.uc_set.get()[:] + self.num_set.get()[:] or S == ' ') and S not in self.cust_sym_dec.get()[:] and S not in s
+        return result
 
     def gen_pwd(self):
         self.save_config()
